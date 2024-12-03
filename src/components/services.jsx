ac7751a9e86946ab0a488b9ce4cc613fd1218158
MdJunayedurRahmanBhuiyan <mbhuiyan@students.oamk.fi>
2024-12-03 22:02:19 +0200

    updated file name service.css

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavbarHome from "../components/NavbarHome";
-import "./Services.css";
+import "./services.css";
 
 const services = [
   {
