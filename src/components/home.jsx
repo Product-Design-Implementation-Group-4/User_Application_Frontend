3a37ef93db7684af9dd0fca2460cb55c1ff86508
MdJunayedurRahmanBhuiyan <mbhuiyan@students.oamk.fi>
2024-12-04 04:18:22 +0200

    file change

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import NavbarHome from "../components/NavbarHome";
 import UserList from "../components/users/UserList";
 import "./Home.css";
 
 function Home() {
-  const navigate = useNavigate();
   return (
     <div>
       <NavbarHome />
       <main>
         <div className="home__helper-box">
           <p>
-            Want to be a Helper? Click to Log In and become a helper. <a href="/login">LOG IN</a>
+            Want to be a Helper? Click to Log In and become a helper. <Link to="/login">LOG IN</Link>
           </p>
           <p>
-            Want to post a Job? Click to Job Post. <a href="/job-post">JOB POST</a>
+            Want to post a Job? Click to Job Post. <Link to="/job-post">JOB POST</Link>
           </p>
         </div>
       
@@ -27,4 +26,4 @@ function Home() {
   );
 }
 
-export default Home; 
+export default Home;
